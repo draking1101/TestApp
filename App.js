f5a7cbbb3b7111e70ef2634922462f2552e118bd
draking1101 <chad30400@gmail.com>
2024-02-11 19:06:20 +0800

    commit

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// 導入元件
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+// UI渲染
 export default function App() {
+  // const food = 'noodles'
+  const [food, setfood] = useState('candy')
+  const [count, setcount] = useState(0)
+  const changefood = () => {
+    setfood('noodles')
+    console.log('已按到setfood')
+  }
+  const changecount = () => {
+    setcount(count + 1)
+    console.log('已按到setcount')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={{ fontSize: 25, color: 'rgb(110,0,0)' }}>Hello World!!</Text>
+      <Text style={{ fontSize: 20, color: '#dfc5ae' }}>食物:{food}</Text>
+      <Text style={{ fontSize: 20, color: '#4ea5da' }}>計數器:{count}</Text>
+      <Button title='Change food' onPress={() => changefood()} />
+      <Button title='Change count' onPress={() => changecount()} />
+    </View >
   );
 }
 
+// 樣式表
 const styles = StyleSheet.create({
   container: {
     flex: 1,
